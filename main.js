1376d01014d1c6eb6a2eaa590c5fcb45e9c33931
Aniuchi Adaobi Michelle <adamichelllle@gmail.com>
2019-09-18 12:45:29 +0100

    Changed structure and design  of the page

--- main.js
+++ main.js
@@ -1,25 +1,40 @@
-/*
-    Carousel
-*/
-$('#projectsDisplayCarousel').on('slide.bs.carousel', function (e) {
-    /*
-        CC 2.0 License Iatek LLC 2018 - Attribution required
-    */
-    var $e = $(e.relatedTarget);
-    var idx = $e.index();
-    var itemsPerSlide = 3;
-    var totalItems = $('.carousel-item').length;
-  console.log(totalItems);
-    if (idx >= totalItems-(itemsPerSlide-1)) {
-        var it = itemsPerSlide - (totalItems - idx);
-        for (var i=0; i<it; i++) {
-            // append slides to end
-            if (e.direction=="left") {
-                $('.carousel-item').eq(i).appendTo('.carousel-inner');
-            }
-            else {
-                $('.carousel-item').eq(0).appendTo('.carousel-inner');
-            }
-        }
-    }
+var lazyLoadInstance = new LazyLoad({
+  elements_selector: ".lazy",
+  threshold: 0,
 });
+
+
+const scroll = window.requestAnimationFrame || function(callback){ window.setTimeout(callback, 1000/60)};
+const elementsToShow = document.querySelectorAll('.show-on-scroll');
+
+// helper function from: http://stackoverflow.com/a/7557433/274826
+function isElementInViewport(el) {
+  // special bonus for those using jQuery
+  if (typeof jQuery === "function" && el instanceof jQuery) {
+    el = el[0];
+  }
+  var rect = el.getBoundingClientRect();
+  return (
+    (rect.top <= 0
+      && rect.bottom >= 0)
+    ||
+    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+    ||
+    (rect.top >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+  );
+}
+
+function loop() {
+    elementsToShow.forEach(function (element) {
+      if (isElementInViewport(element)) {
+        element.classList.add('is-visible');
+      } else {
+        element.classList.remove('is-visible');
+      }
+    });
+  
+    scroll(loop);
+}
+loop();
